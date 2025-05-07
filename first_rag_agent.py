f1d9b8b7902e6d66c86c97bd840f92177125567c
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-07 16:23:15 -0400

    Chose Google as Chatbot and Vector Embedder provider

--- first_rag_agent.py
+++ first_rag_agent.py
@@ -1,5 +1,16 @@
 import getpass
 import os
 
+# Set LangSmith Environment Variables
 os.environ["LANGSMITH_TRACING"] = "true"
 os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
+
+
+# Set up google llm model for RAG usage
+os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+from langchain.chat_models import init_chat_model
+llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+from langchain_google_vertexai import VertexAIEmbeddings
+
+embeddings = VertexAIEmbeddings(model="text-embedding-004")
