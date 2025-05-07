a58d59e822bb670fc2138201ef93b82a5f23109e
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-07 16:26:18 -0400

    Added commits to assist in the llm - rag agent creation process

--- first_rag_agent.py
+++ first_rag_agent.py
@@ -9,8 +9,12 @@ os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
 # Set up google llm model for RAG usage
 os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 from langchain.chat_models import init_chat_model
+print("Initializaing chat model!")
 llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
-
+print("Chat model initialization over!")
 from langchain_google_vertexai import VertexAIEmbeddings
 
+
+print("Initializaing embedder model!")
 embeddings = VertexAIEmbeddings(model="text-embedding-004")
+print("Embedder model initialized!")
