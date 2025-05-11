a276d5ff04eb98170eeed6c4c752f9a77c49cadf
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-11 13:35:52 -0400

    Decided upon my chat model, embedding model, and my vector store

--- first_rag_agent.py
+++ first_rag_agent.py
@@ -3,18 +3,28 @@ import os
 
 # Set LangSmith Environment Variables
 os.environ["LANGSMITH_TRACING"] = "true"
-os.environ["LANGSMITH_API_KEY"] = getpass.getpass()
+os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter API key for LangSmith")
 
 
 # Set up google llm model for RAG usage
 os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
+
+
+print("Initializing chat model!")
 from langchain.chat_models import init_chat_model
-print("Initializaing chat model!")
+
 llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 print("Chat model initialization over!")
-from langchain_google_vertexai import VertexAIEmbeddings
 
 
-print("Initializaing embedder model!")
-embeddings = VertexAIEmbeddings(model="text-embedding-004")
+print("Initializing embedder model!")
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 print("Embedder model initialized!")
+
+print("Initialize vector store!")
+from langchain_core.vectorstores import InMemoryVectorStore
+
+vector_store = InMemoryVectorStore(embeddings)
+print("Vector store initialized!")
