4c3c35ae9fe06f22f1ca6b68b17ef1b64abd300d
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-23 10:49:03 -0400

    Got very basic streamlit app with text input working

--- rag_agent_streamlit_app.py
+++ rag_agent_streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title("huh?")
 st.write("yo")
+
+api_key = ""
+
+with st.sidebar:
+  st.header("HUH?")
+  api_key = st.text_input("Yo???? Input???", type="password", help="Yo what's up?")
+  
+
+print(f"bruh {api_key}")
