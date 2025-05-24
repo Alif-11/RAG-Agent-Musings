8c372f4f35eb9332e1f06656a7e6768bcd69b231
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-24 09:33:10 -0400

    Added text fields for users to input api keys for langsmith and google gemini, as well as text inputs for users to write queries about the website they had questions on

--- rag_agent_streamlit_app.py
+++ rag_agent_streamlit_app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
-st.title("huh?")
-st.write("yo")
+st.title("RAG Agent")
+website_to_scrape_link = st.text_input("Input The Website You Want The Agent To Scrape For Context", type="default")
+user_query = st.text_input("Input Your Query Here", type="default")
 
 api_key = ""
 
 with st.sidebar:
-  st.header("HUH?")
-  api_key = st.text_input("Yo???? Input???", type="password", help="Yo what's up?")
+  st.header("API Keys")
+  langsmith_api_key = st.text_input("LangSmith API Key", type="password", help="A LangSmith API Key is required to use this app.")
+  google_gemini_api_key = st.text_input("Google Gemini Flash 2.0 API Key", type="password", help="A Google Gemini Flash 2.0 API Key is required to use this app.")
   
+if langsmith_api_key != "":
+  print(f"langsmith api key: {langsmith_api_key}")
 
-print(f"bruh {api_key}")
+if google_gemini_api_key != "":
+  print(f"google gemini api key: {google_gemini_api_key}")
